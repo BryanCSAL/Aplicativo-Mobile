04c3d0682177dc6c2cc56c9cf29dd13208b457fb
BCSAL <bryan.c.s.a.lopes@hotmail.com>
2024-12-13 21:19:34 -0300

    Atualizando

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.moblieproject"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.moblieproject"
